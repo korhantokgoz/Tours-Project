85778f59b9a0a2479dfe70424f6a507370b80d52
Sam Smith <korhan.tokgoz92@gmail.com>
2022-06-04 01:24:37 +0300

    Updates

---
+++ precache-manifest.0d87ff0af691968998584c04348d9082.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "228913469294ce411b3d92c88022c8c4",
+    "url": "/Tours-Project/index.html"
+  },
+  {
+    "revision": "c597cf56ad85c8532a0c",
+    "url": "/Tours-Project/static/css/main.c3e53c0d.chunk.css"
+  },
+  {
+    "revision": "303a7e45ea08ea333814",
+    "url": "/Tours-Project/static/js/2.b79bb511.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Tours-Project/static/js/2.b79bb511.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c597cf56ad85c8532a0c",
+    "url": "/Tours-Project/static/js/main.8ebbf793.chunk.js"
+  },
+  {
+    "revision": "12396e6f810bb4f0c0f1",
+    "url": "/Tours-Project/static/js/runtime-main.8e4f5b5c.js"
+  }
+]);
